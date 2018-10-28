327ae5ae5173787ee25332c41f50f94d9cc4cad2
juano24 <juanitoprdomo@gmail.com>
2018-10-28 18:20:25 -0500

    Porbando los commit

---
+++ index - copia.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="content">
+	<meta name="keywords" content="content">
+	<meta name="author" content="content">
+	<title>Iglesia Wesleyana Casa de Dios</title>
+	<link rel="shortcut icon" href="assets/img/iconos/favicon.ico">
+	<link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/mystyle.css">
+</head>
+<body>
+
+
+	<!--AQUI INICIA LA CABECERA Y MENU DE NAVEGACION-->	
+	<header>
+		<nav>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			  <a class="navbar-brand" href="#">
+			  	<img src="assets/img/iconos/Logogris.png" alt="" width="350" class="img-fluid ml-md-4 ml-lg-4 ml-xl-4">
+			  </a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+
+			  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    <ul class="navbar-nav ml-auto font-weight-bold">
+			      <li class="nav-item active">
+			        <a class="nav-link" href="index.php">INICIO<span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="quienesosmos.php">QUIENES SOMOS</a>
+			      </li>
+			      <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ESCUELAS</a>
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="#">CONSOLIDACIÓN</a>
+			          <a class="dropdown-item" href="#">DISCIPULADO</a>
+			          <!--<div class="dropdown-divider"></div>-->
+			          <a class="dropdown-item" href="#">LIDERAZGO</a>
+			        </div>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">MINISTERIOS</a>
+			      </li>
+			      <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">MULTIMEDIA</a>
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="#">FOTOS</a>
+			          <a class="dropdown-item" href="#">VIDEOS</a>
+			          <a class="dropdown-item" href="#">AUDIOS</a>
+			        </div>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">CONTACTO</a>
+			      </li>
+			      <!--<li class="nav-item">
+			        <a class="nav-link disabled" href="#">Disabled</a>
+			      </li>-->
+			    </ul>
+			  </div>
+			</nav>
+		</nav>
+	</header><!-- /header -->
+	<!--AQUI TERMINA CABECERA Y MENU DE NAVEGACION-->
+
+
+	<!--AQUI INICIA SLIDE INFORMATIVO-->
+	<section>
+		<div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+		  <ol class="carousel-indicators">
+		    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		  </ol>
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img class="d-block w-100" src="assets/img/carrusel/slide1.png" alt="First slide">
+		      <div class="carousel-caption d-none d-md-block">
+				    <h5>Lorem ipsum dolor.</h5>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, similique.</p>
+				  </div>
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="assets/img/carrusel/slide2.png" alt="Second slide">
+		      <div class="carousel-caption d-none d-md-block">
+				    <h5>Lorem ipsum dolor.</h5>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, similique.</p>
+				  </div>
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="assets/img/carrusel/slide3.png" alt="Third slide">
+		      <div class="carousel-caption d-none d-md-block">
+				    <h5>Lorem ipsum dolor.</h5>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, similique.</p>
+				  </div>
+		    </div>
+		  </div>
+		  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+	</section>
+	<!--AQUI TERMINA EL SLIDE INFORMATIVO-->
+
+
+	<!--INICIO SECCION-->
+	<section>
+		<div class="container-fluid mt-5 p-5">
+			<div class="row">
+				<div class="col-12 col-ms-12 col-md-4 col-lg-4 col-xl-4">
+					<h3 class="text-center font-weight-bold mb-4">Lorem ipsum dolor.</h3>
+					<img src="assets/img/imagenes/img1.jpg" alt="" class="img-fluid mx-auto d-block rounded-circle mb-4 shadow">
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quae nemo distinctio laborum voluptates minus ex tenetur beatae architecto, corrupti saepe dolorum doloribus vero unde sit rem fugit accusamus ab perferendis! Iure voluptates maiores dolorem sunt ullam minima quae fuga cum placeat magni sed earum, accusamus mollitia numquam sint iusto laborum, eligendi eum, molestias! Cumque amet numquam optio enim qui ut voluptate placeat ad labore vitae, nulla velit dolorem expedita architecto necessitatibus corporis hic pariatur, sit voluptatum eum, totam ipsum voluptas doloribus deleniti. Saepe ut dolore perferendis, nulla. Voluptatum asperiores enim sequi neque, quasi qui repellendus magni voluptate nobis dolorem.</p>
+					<button type="button" class="btn-primary font-weight-bold rounded">Ver mas</button>
+				</div>
+				<div class="col-12 col-ms-12 col-md-4 col-lg-4 col-xl-4">
+					<h3 class="text-center font-weight-bold mb-4">Lorem ipsum dolor.</h3>
+					<img src="assets/img/imagenes/img1.jpg" alt="" class="img-fluid mx-auto d-block rounded-circle mb-4 shadow">
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quae nemo distinctio laborum voluptates minus ex tenetur beatae architecto, corrupti saepe dolorum doloribus vero unde sit rem fugit accusamus ab perferendis! Iure voluptates maiores dolorem sunt ullam minima quae fuga cum placeat magni sed earum, accusamus mollitia numquam sint iusto laborum, eligendi eum, molestias! Cumque amet numquam optio enim qui ut voluptate placeat ad labore vitae, nulla velit dolorem expedita architecto necessitatibus corporis hic pariatur, sit voluptatum eum, totam ipsum voluptas doloribus deleniti. Saepe ut dolore perferendis, nulla. Voluptatum asperiores enim sequi neque, quasi qui repellendus magni voluptate nobis dolorem.</p>
+					<button type="button" class="btn-primary font-weight-bold rounded">Ver mas</button>
+				</div>
+				<div class="col-12 col-ms-12 col-md-4 col-lg-4 col-xl-4">
+					<h3 class="text-center font-weight-bold mb-4">Lorem ipsum dolor.</h3>
+					<img src="assets/img/imagenes/img1.jpg" alt="" class="img-fluid mx-auto d-block rounded-circle mb-4 shadow">
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quae nemo distinctio laborum voluptates minus ex tenetur beatae architecto, corrupti saepe dolorum doloribus vero unde sit rem fugit accusamus ab perferendis! Iure voluptates maiores dolorem sunt ullam minima quae fuga cum placeat magni sed earum, accusamus mollitia numquam sint iusto laborum, eligendi eum, molestias! Cumque amet numquam optio enim qui ut voluptate placeat ad labore vitae, nulla velit dolorem expedita architecto necessitatibus corporis hic pariatur, sit voluptatum eum, totam ipsum voluptas doloribus deleniti. Saepe ut dolore perferendis, nulla. Voluptatum asperiores enim sequi neque, quasi qui repellendus magni voluptate nobis dolorem.</p>
+					<button type="button" class="btn-primary font-weight-bold rounded">Ver mas</button>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--TERMINA SECCION-->
+
+	
+	<!--INICIA SECCION 2-->
+	<section>
+		<div class="container-fluid bg-light p-5">
+			<div class="row">
+				<div class="col-12 col-ms-12 col-md-8 col-lg-8 col-xl-8">
+					<div class="embed-responsive embed-responsive-16by9">
+  					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/OceNj5XHywA" allowfullscreen></iframe>
+					</div>
+				</div>
+				<div class="col-12 col-ms-12 col-md-4 col-lg-4 col-xl-4">
+					<h3 class="text-center font-weight-bold mb-4">Lorem ipsum dolor.</h3>
+					<img src="assets/img/imagenes/img1.jpg" alt="" class="img-fluid mx-auto d-block rounded-circle mb-4 shadow">
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis ullam nemo itaque excepturi suscipit unde repudiandae nesciunt ad voluptates minima recusandae illum exercitationem, neque, ut totam ratione. Consequuntur consequatur ad, nesciunt nulla, voluptate voluptates qui, natus labore facilis dolore odit, vero ea sint inventore tenetur et eligendi nobis fugit veniam.</p>
+					<button type="button" class="btn-primary font-weight-bold rounded">Ver mas</button>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--TERMINA SECCION 2-->
+
+
+	<!--INICIA PIE DE PAGINA-->	
+	<footer class="bg-secondary">
+		<div class="container-fluid p-5 text-light">
+			<div class="row">
+				<div class=" col-12 col-ms-12 col-md-4 col-lg-4 col-xl-4">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi ducimus, dolor ab obcaecati temporibus asperiores ad dignissimos dolorum unde fuga quae voluptates beatae quasi, voluptatum. Ducimus incidunt beatae rem, omnis! Quisquam nobis, pariatur nulla expedita aliquid repellendus laudantium voluptates aliquam quasi voluptas repellat placeat a, illum. Facilis, eos quas possimus.</p>
+				</div>
+				<div class=" col-12 col-ms-12 col-md-4 col-lg-4 col-xl-4">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates ab, obcaecati, eaque, accusamus dolorem a beatae mollitia quod et quae non ducimus quam aperiam vel cum quisquam enim reprehenderit sunt cupiditate ullam id quidem perspiciatis dolore molestiae. Amet dolore fuga, voluptate deleniti placeat beatae repellendus, asperiores exercitationem illo eaque libero.</p>
+				</div>
+				<div class=" col-12 col-ms-12 col-md-4 col-lg-4 col-xl-4">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto minus cupiditate eum quo quasi laboriosam tempore veniam repudiandae dolor aperiam iste porro amet odio eius earum tempora commodi laborum cumque, impedit nam eligendi mollitia ex nobis suscipit. Nihil consequatur accusantium asperiores fugit eos corporis odit est quae amet, iure, voluptatem.</p>
+				</div>
+			</div>
+		</div>
+		<div class="bg-dark text-light p-2">
+			<h5 class="font-weight-bold text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, facere.</h5>
+		</div>
+	</footer>
+	<!-- TERMINA EL PIE DE PAGIA -->
+
+
+	<!--------------------SCRIPS-------------------->
+	<script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/bootstrap.bundle.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
