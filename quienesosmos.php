5d64c7b26b04b11840afbaba3d2935edffbe7cd7
juano24 <juanitoprdomo@gmail.com>
2018-10-28 17:20:15 -0500

    Iniciando pagina Web

---
+++ quienesosmos.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="content">
+	<meta name="keywords" content="content">
+	<meta name="author" content="content">
+	<title> Quienes somos |Casa de Dios</title>
+	<link rel="shortcut icon" href="assets/img/iconos/favicon.ico">
+	<link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/mystyle.css">
+</head>
+<body>
+
+
+	<!--AQUI INICIA LA CABECERA Y MENU DE NAVEGACION-->	
+	<header>
+		<nav>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			  <a class="navbar-brand" href="#">
+			  	<img src="assets/img/iconos/Logogris.png" alt="" width="350" class="img-fluid ml-md-4 ml-lg-4 ml-xl-4">
+			  </a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+
+			  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    <ul class="navbar-nav ml-auto font-weight-bold">
+			      <li class="nav-item active">
+			        <a class="nav-link" href="index.php">INICIO<span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="quienesosmos.php">QUIENES SOMOS</a>
+			      </li>
+			      <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ESCUELAS</a>
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="#">CONSOLIDACIÓN</a>
+			          <a class="dropdown-item" href="#">DISCIPULADO</a>
+			          <!--<div class="dropdown-divider"></div>-->
+			          <a class="dropdown-item" href="#">LIDERAZGO</a>
+			        </div>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">MINISTERIOS</a>
+			      </li>
+			      <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">MULTIMEDIA</a>
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="#">FOTOS</a>
+			          <a class="dropdown-item" href="#">VIDEOS</a>
+			          <a class="dropdown-item" href="#">AUDIOS</a>
+			        </div>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">CONTACTO</a>
+			      </li>
+			      <!--<li class="nav-item">
+			        <a class="nav-link disabled" href="#">Disabled</a>
+			      </li>-->
+			    </ul>
+			  </div>
+			</nav>
+		</nav>
+	</header><!-- /header -->
+	<!--AQUI TERMINA CABECERA Y MENU DE NAVEGACION-->
+
+
+	<!--AQUI INICIA SLIDE INFORMATIVO-->
+	
+	<!--AQUI TERMINA EL SLIDE INFORMATIVO-->
+
+
+	<!--INICIO SECCION-->
+	
+	<!--TERMINA SECCION-->
+
+	
+	<!--INICIA SECCION 2-->
+	
+	<!--TERMINA SECCION 2-->
+
+
+	<!--INICIA PIE DE PAGINA-->	
+	<footer class="bg-secondary">
+		<div class="container-fluid p-5 text-light">
+			<div class="row">
+				<div class=" col-12 col-ms-12 col-md-4 col-lg-4 col-xl-4">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi ducimus, dolor ab obcaecati temporibus asperiores ad dignissimos dolorum unde fuga quae voluptates beatae quasi, voluptatum. Ducimus incidunt beatae rem, omnis! Quisquam nobis, pariatur nulla expedita aliquid repellendus laudantium voluptates aliquam quasi voluptas repellat placeat a, illum. Facilis, eos quas possimus.</p>
+				</div>
+				<div class=" col-12 col-ms-12 col-md-4 col-lg-4 col-xl-4">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates ab, obcaecati, eaque, accusamus dolorem a beatae mollitia quod et quae non ducimus quam aperiam vel cum quisquam enim reprehenderit sunt cupiditate ullam id quidem perspiciatis dolore molestiae. Amet dolore fuga, voluptate deleniti placeat beatae repellendus, asperiores exercitationem illo eaque libero.</p>
+				</div>
+				<div class=" col-12 col-ms-12 col-md-4 col-lg-4 col-xl-4">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto minus cupiditate eum quo quasi laboriosam tempore veniam repudiandae dolor aperiam iste porro amet odio eius earum tempora commodi laborum cumque, impedit nam eligendi mollitia ex nobis suscipit. Nihil consequatur accusantium asperiores fugit eos corporis odit est quae amet, iure, voluptatem.</p>
+				</div>
+			</div>
+		</div>
+		<div class="bg-dark text-light p-2">
+			<h5 class="font-weight-bold text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, facere.</h5>
+		</div>
+	</footer>
+	<!-- TERMINA EL PIE DE PAGIA -->
+
+
+	<!--------------------SCRIPS-------------------->
+	<script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/bootstrap.bundle.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
